bddbf451f4a3d6fedd13a1543f40e16f0b132fca
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-03-20 22:33:04 -0400

    update

--- Assets/app.js
+++ Assets/app.js
@@ -1,29 +1,57 @@
 $(document).ready(function () {
+    //Array of the rows
+    var divs = ["nine", "ten", "eleven", "twelve", "one", "two", "three", "four", "five"]
+    //Array of the textareas
+    var textAreas = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
+
+
     //This will display the current date
- $("#currentDay").text(moment().format("dddd , MMM Do YYYY"));
+    $("#currentDay").text(moment().format("dddd , MMM Do YYYY"))
+}, 1000)
+
+//retrieve and save time to set variables
+var today = new Date();
+var hour = today.getHours();
+
+//loop to run through each textarea of each hour
+for (let index = 0; index < textAreas.length; index++) {
+    var data = $(textAreas[i]).attr("data-time");
+
+    //changes the string into a integer
+    data = parseInt(data);
+
+    //conditionals
+    if (hour < data) {
+        $(textAreas[i].addclass("col-md-10 Savedtext"))
+    }
+    if (hour === data) {
+        $(textAreas[i].addclass("col-md-10 Savedtext"))
+    }
+    if (hour > data) {
+        $(textAreas[i].addclass("col-md-10 Savedtext"))
+    }
+}
 
 //function for when you press the save button
- $(".saveBtn").on("click" , function(){
- var value = $(this).siblings(".Savedtext").val();
- var time = $(this).parent().attr("id")
- //save item in local storage
- localStorage.setItem(time, value);
+$(".saveBtn").on("click", function () {
+    var value = $(this).siblings(".Savedtext").val();
+    var time = $(this).parent().attr("id")
+    //save item in local storage
+    localStorage.setItem(time, value);
 });
-//Array of the rows
-var divs = ["nine", "ten", "eleven", "twelve", "one", "two", "three", "four", "five"]
-//Array of the textareas
-var textAreas =["9", "10", "11", "12", "1", "2", "3", "4", "5"]
+
 
 //for loop that will run through each string in the array and if there is something saved it will add to text area but if
 // nothing is saved it will equal null
 for (let i = 0; i < divs.length; i++) {
     var savedData = localStorage.getItem(divs[i]);
-    if (savedData === null){
+    if (savedData === null) {
         console.log(textAreas)
     }
-    else{
-        $("#"+ textAreas[i]).text(savedData);
+    else {
+        $("#" + textAreas[i]).text(savedData);
         console.log(savedData)
     }
+
+
 }
-});
