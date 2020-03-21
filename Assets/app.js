9ff8d2e9dcfef95828bbf1a383cca00276b97b29
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-03-20 23:14:00 -0400

    finished product

--- Assets/app.js
+++ Assets/app.js
@@ -6,44 +6,45 @@ $(document).ready(function () {
 
 
     //This will display the current date
+    setInterval(function(){
     $("#currentDay").text(moment().format("dddd , MMM Do YYYY"))
-}, 1000)
+   }, 1000)
 
-//retrieve and save time to set variables
-var today = new Date();
-var hour = today.getHours();
-
-//loop to run through each textarea of each hour
-for (let index = 0; index < textAreas.length; index++) {
+ //retrieve and save time to set variables
+ var today = new Date();
+ var hour = today.getHours();
+ console.log(hour)
+ //loop to run through each textarea of each hour
+ for (var i = 0; i < textAreas.length; i++) {
     var data = $(textAreas[i]).attr("data-time");
 
     //changes the string into a integer
-    data = parseInt(data);
+    data= parseInt(data);
 
     //conditionals
     if (hour < data) {
-        $(textAreas[i].addclass("col-md-10 Savedtext"))
+        $(textAreas[i]).addclass("col-md-10 future Savedtext")
     }
     if (hour === data) {
-        $(textAreas[i].addclass("col-md-10 Savedtext"))
+        $(textAreas[i]).addclass("col-md-10 present Savedtext")
     }
     if (hour > data) {
-        $(textAreas[i].addclass("col-md-10 Savedtext"))
+        $(textAreas[i]).addclass("col-md-10 past Savedtext")
     }
-}
+ }
 
-//function for when you press the save button
-$(".saveBtn").on("click", function () {
+ //function for when you press the save button
+ $(".saveBtn").on("click", function () {
     var value = $(this).siblings(".Savedtext").val();
     var time = $(this).parent().attr("id")
     //save item in local storage
     localStorage.setItem(time, value);
-});
+ });
 
 
-//for loop that will run through each string in the array and if there is something saved it will add to text area but if
-// nothing is saved it will equal null
-for (let i = 0; i < divs.length; i++) {
+ //for loop that will run through each string in the array and if there is something saved it will add to text area but if
+ // nothing is saved it will equal null
+ for (let i = 0; i < divs.length; i++) {
     var savedData = localStorage.getItem(divs[i]);
     if (savedData === null) {
         console.log(textAreas)
@@ -54,4 +55,5 @@ for (let i = 0; i < divs.length; i++) {
     }
 
 
-}
+ }
+});
